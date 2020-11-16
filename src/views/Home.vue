cebc17df0d0cf9733e29db2bc9f50eae177585c1
Harris - BESNARD CONSULTING <contact@besnard-consulting.com>
2020-11-16 13:47:15 +0800

    AREM-5, Add Series Components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,66 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <SeriesList v-bind:series="series" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import SeriesList from "@/components/SeriesList";
 export default {
   name: 'Home',
   components: {
+    SeriesList
+  },
+  data() {
+    return {
+      series: [
+        {
+          "id": 1,
+          "title": "Django Testing",
+          "status": "Not started",
+          "items": [
+            {
+              "id": 1,
+              "name": "Script Writing",
+              "status": "Done"
+            },
+            {
+              "id": 2,
+              "name": "Video Recording",
+              "status": "Done"
+            },
+            {
+              "id": 3,
+              "name": "Article Writing",
+              "status": "In Progress"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "title": "Git Series",
+          "status": "In Progress",
+          "items": [
+            {
+              "id": 1,
+              "name": "Script Writing",
+              "status": "Done"
+            },
+            {
+              "id": 2,
+              "name": "Video Recording",
+              "status": "Done"
+            },
+            {
+              "id": 3,
+              "name": "Article Writing",
+              "status": "In Progress"
+            }
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
