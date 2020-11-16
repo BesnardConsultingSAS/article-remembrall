f016460608d51401606f4612082f397796592088
Harris - BESNARD CONSULTING <contact@besnard-consulting.com>
2020-11-16 13:47:15 +0800

    AREM-5, Add Series Components

---
+++ src/components/SeriesItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <div class="d-flex justify-content-between">
+        <a href="">{{ series.title }}</a>
+        <span class="badge badge-pill badge-warning">
+          {{ series.status }}
+        </span>
+      </div>
+
+    </div>
+    <div class="card-body">
+      <div v-bind:key="item.id" v-for="item in series.items">
+        <p>{{ item.name }} <span class="badge badge-pill badge-warning"> {{ item.status }}</span></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SeriesItem",
+  props: ["series"],
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SeriesList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex flex-row-reverse">
+    <button class="btn btn-primary">ADD SERIES</button>
+  </div>
+
+  <div v-bind:key="item.id" v-for="item in series" class="seriesList">
+    <SeriesItem v-bind:series="item" />
+  </div>
+
+</template>
+
+<script>
+import SeriesItem from "@/components/SeriesItem";
+
+export default {
+  name: "SeriesList",
+  components: {
+    SeriesItem
+  },
+  props: ["series"]
+}
+</script>
+
+<style scoped>
+.seriesList {
+  margin-top: 2em;
+}
+</style>
